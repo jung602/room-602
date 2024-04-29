3dd88b99831e2ff13ce7edccea68e0b00a542553
jung602 <kuhaku05@naver.com>
2024-04-29 11:03:02 +0900

    0429

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
-import Head from 'next/head'
  
 
 const inter = Inter({ subsets: ['latin'] })
