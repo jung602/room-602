54c7a4bee46472f9920b95ee8954f061d1211f99
jung602 <kuhaku05@naver.comm>
2024-05-01 13:53:45 +0900

    0501

--- app/components/pngSquence.tsx
+++ app/components/pngSquence.tsx
@@ -1,36 +1,44 @@
 import React, { useState, useEffect } from 'react';
 
 interface PngSequenceProps {
-    isActive: boolean; // isActive 프로퍼티 추가
-    
+    isActive: boolean;
     className?: string;
-  }
-  
+}
 
-const PngSequenceAnimation: React.FC<PngSequenceProps> = ({isActive, className}) => {
+const PngSequenceAnimation: React.FC<PngSequenceProps> = ({ isActive, className }) => {
     const [frame, setFrame] = useState<number>(1);
+    const numFrames = 60; // 총 프레임 수 정의
     const animationSpeed = 1000 / 30; // 30 FPS
-  
+
+    // 프리로딩 로직
     useEffect(() => {
-      let animationFrameId: ReturnType<typeof setTimeout>;
-  
-      if (!isActive && frame < 30) {
-        animationFrameId = setTimeout(() => setFrame(frame + 1), animationSpeed);
-      } else if (isActive && frame < 60) {
-        animationFrameId = setTimeout(() => setFrame(frame + 1), animationSpeed);
-      } else if (!isActive && frame > 30) {
-        animationFrameId = setTimeout(() => setFrame(frame - 1), animationSpeed);
-      }
-  
-      return () => clearTimeout(animationFrameId);
+        for (let i = 1; i <= numFrames; i++) {
+            const img = new Image();
+            img.src = `./trainerCard/TrainerCard${i.toString().padStart(5, '0')}.png`;
+            img.onload = () => console.log(`Preloaded image ${i}`);
+        }
+    }, []);
+
+    // 애니메이션 로직
+    useEffect(() => {
+        if (!isActive && frame < 30) {
+            const timer = setTimeout(() => setFrame(frame + 1), animationSpeed);
+            return () => clearTimeout(timer);
+        } else if (isActive && frame < 60) {
+            const timer = setTimeout(() => setFrame(frame + 1), animationSpeed);
+            return () => clearTimeout(timer);
+        } else if (!isActive && frame > 30) {
+            const timer = setTimeout(() => setFrame(frame - 1), animationSpeed);
+            return () => clearTimeout(timer);
+        }
     }, [frame, isActive]);
-  
+
     const formattedFrame = frame.toString().padStart(5, '0');
     const imagePath = `./trainerCard/TrainerCard${formattedFrame}.png`;
-  
+
     return (
         <img className={className} src={imagePath} alt={`Frame ${frame}`} />
     );
-  };
-  
-  export default PngSequenceAnimation;
+};
+
+export default PngSequenceAnimation;
