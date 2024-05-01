105b6443a5f4466b9a7b5474989a1bfb34c85920
jung602 <kuhaku05@naver.comm>
2024-05-01 16:34:05 +0900

    0501

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,32 @@
 import Image from 'next/image'
 import './globals.css'
 import sassy from './sassy.module.scss'
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MagneticTabs } from './components/menu'
-
- 
 
 export default function Home() {
   const [activeTabId, setActiveTabId] = useState<number | null>(null);
+  const tabsRef = useRef<HTMLDivElement>(null); // Ref for the tabs container with a specific type
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // Check if the click target is a node and not contained within the tabsRef element
+      if (tabsRef.current && !tabsRef.current.contains(event.target as Node)) {
+        setActiveTabId(null); // Reset the active tab if clicked outside of the tabs
+      }
+    };
+
+    // Add the event listener to the document
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Remove the event listener on cleanup
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount and cleanup on unmount
 
   return (
       <>
-          <main className={sassy.main} 
-          /**style={{ backgroundColor: getBackgroundColor(activeTabId)}}*/>
+          <main className={sassy.main}>
           <div className={sassy.top}>
            <div>
             <div className={sassy.image}>
@@ -30,7 +43,7 @@ export default function Home() {
             <span> for a bit.</span>
             </div>
           </div>
-          <div className={sassy.contents}>
+          <div className={sassy.contents} ref={tabsRef}>
           <MagneticTabs setActiveTabId={setActiveTabId} />
           </div>
           <div className={sassy.bottom}>
@@ -38,6 +51,5 @@ export default function Home() {
           </div>
           </main>
       </>
-
   )
 }
