a026a324a99a7649d3ae24dafb71add019b37d66
jung602 <kuhaku05@naver.comm>
2024-05-02 23:51:57 +0900

    0502

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import styles from './load.module.scss'
 
 
 export default function Loading() {
     return(
-            <SkeletonTheme baseColor="#990" highlightColor="#550">
-                {" "}
-                Loading...
+            <SkeletonTheme>
+                    <>
+                        <div>Loading</div>
+                    </>
+
             </SkeletonTheme>
     );
 }
