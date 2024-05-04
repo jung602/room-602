49c4782f371d69f9473dfbe535edaa2ce7a26e45
jung602 <kuhaku05@naver.comm>
2024-05-05 03:04:58 +0900

    0505

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Head from 'next/head'
 import Image from 'next/image'
 import './globals.css'
 import sassy from './sassy.module.scss'
@@ -12,6 +13,26 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+
+          <title>Room602</title>
+          <link rel="icon" href="https://erin-jung.com/favicon.ico" sizes="any" />
+          <meta name="title" content="Room602" />
+          <meta name="description" content="Virtual Designer Based in Delulu" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://erin-jung.com/" />
+          <meta property="og:title" content="Room602" />
+          <meta property="og:description" content="Virtual Designer Based in Delulu" />
+          <meta property="og:image" content="https://erin-jung.com/og.jpg" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://erin-jung.com/" />
+          <meta property="twitter:title" content="Room602" />
+          <meta property="twitter:description" content="Virtual Designer Based in Delulu" />
+          <meta property="twitter:image" content="https://erin-jung.com/og.jpg" />
+
+      </Head>
       <main className={`${sassy.main} ${"Semibold"}` }>
             <div className={sassy.top}>
               <div>
